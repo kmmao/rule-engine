45d5111bd8a2613df80bc710a6daf9d8f1d08669
dqw <761945125@qq.com>
2020-11-18 23:53:45 +0800

    增加字符串替换函数ã

---
+++ rule-engine-web/src/main/java/com/engine/web/function/StringReplaceFunction.java
@@ -0,0 +1,46 @@
+package com.engine.web.function;
+
+import com.engine.core.annotation.Executor;
+import com.engine.core.annotation.Function;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author 丁乾文
+ * @create 2020/11/18
+ * @since 1.0.0
+ */
+@Slf4j
+@Function
+public class StringReplaceFunction {
+
+    @Executor
+    public String executor(@Valid Params params) {
+        String value = params.getValue();
+        if (value == null) {
+            return null;
+        }
+        String replacement = params.getReplacement();
+        return value.replace(params.getTarget(), replacement);
+    }
+
+    @Data
+    public static class Params {
+
+        private String value;
+
+        @NotNull
+        private String target;
+
+        @NotNull
+        private String replacement;
+
+    }
+
+}
