2398feebf751f9da7812b9539333dc2384f32b13
dqw <761945125@qq.com>
2020-12-21 03:37:10 +0800

    add monitor

--- rule-engine-monitor/bin/startup.sh
+++ rule-engine-monitor/bin/startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash/
-nohup java -jar -server -Xms512m -Xmx512m -Xss256k -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dfile.encoding=UTF-8 rule-engine-monitor-1.0-SNAPSHOT.jar --server.port=8011 &
+nohup java -jar -server -Xms512m -Xmx512m -Xss256k -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -Dfile.encoding=UTF-8 rule-engine-monitor-1.1-SNAPSHOT.jar --server.port=8011 &
--- rule-engine-web/src/main/java/cn/ruleengine/web/App.java
+++ rule-engine-web/src/main/java/cn/ruleengine/web/App.java
@@ -22,7 +22,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -54,8 +53,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableScheduling
 @EnableCaching(order = -2)
 @SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        SecurityAutoConfiguration.class
+        DataSourceAutoConfiguration.class
 },
         scanBasePackages = "cn.ruleengine")
 @MapperScan({"cn.ruleengine.web.store.mapper"})
