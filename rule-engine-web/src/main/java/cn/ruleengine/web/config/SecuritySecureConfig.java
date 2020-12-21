34047706829fa2305380b7b89b4596ca7713b253
dqw <761945125@qq.com>
2020-12-21 16:11:13 +0800

    已知问题修复

---
+++ rule-engine-web/src/main/java/cn/ruleengine/web/config/SecuritySecureConfig.java
@@ -0,0 +1,26 @@
+package cn.ruleengine.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author dingqianwen
+ * @date 2020/12/21
+ * @since 1.0.0
+ */
+@Configuration
+public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and().logout().permitAll();
+    }
+
+}
