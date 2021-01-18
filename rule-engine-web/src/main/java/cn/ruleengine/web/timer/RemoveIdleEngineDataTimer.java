32365852d720094b627f9d842d05b5e07b019d21
dqw <761945125@qq.com>
2021-01-18 15:50:31 +0800

    代码优化

---
+++ rule-engine-web/src/main/java/cn/ruleengine/web/timer/RemoveIdleEngineDataTimer.java
@@ -0,0 +1,19 @@
+package cn.ruleengine.web.timer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ * 移除引擎中长时间未使用规则/决策表/规则集
+ *
+ * @author dingqianwen
+ * @date 2021/1/18
+ * @since 1.0.0
+ */
+@Slf4j
+@Component
+public class RemoveIdleEngineDataTimer {
+
+}
