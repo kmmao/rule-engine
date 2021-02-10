0fe4ffb3f0e535d3a2639d88a4794a08c684fb91
dqw <761945125@qq.com>
2021-02-10 19:15:44 +0800

    代码优化

--- rule-engine-web/src/main/java/cn/ruleengine/web/config/rabbit/RabbitConfig.java
+++ rule-engine-web/src/main/java/cn/ruleengine/web/config/rabbit/RabbitConfig.java
@@ -5,7 +5,6 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
